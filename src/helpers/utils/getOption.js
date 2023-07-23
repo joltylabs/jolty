abd44ca18556ecbeaacabeae15e8562468174799
Anatolii Moldovanov <anmoldovanovdev@gmail.com>
2023-07-23 21:38:53 +0200

    remove getOption isHTML check

--- src/helpers/utils/getOption.js
+++ src/helpers/utils/getOption.js
@@ -1,18 +1,12 @@
 import { returnArray, callOrReturn } from "./index.js";
-import { isString, isHTML } from "../is";
-import { fragment } from "../dom";
+import { isString } from "../is";
 import { doc } from "../constants";
 
 export default function (multiply, instance, option, root = doc, ...params) {
   if (!option) return;
   option = callOrReturn(option, instance, ...params);
   if (isString(option)) {
-    if (isHTML(option)) {
-      option = fragment(option);
-    } else {
-      option = root[multiply ? "querySelectorAll" : "querySelector"](option);
-    }
+    option = root[multiply ? "querySelectorAll" : "querySelector"](option);
   }
-
   return multiply ? returnArray(option).filter(Boolean) : option;
 }
