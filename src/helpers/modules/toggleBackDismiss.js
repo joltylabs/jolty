9a710fa0ce9920326665c74924c0e40c149f5601
Anatolii Moldovanov <anmoldovanovdev@gmail.com>
2023-11-10 23:19:45 +0100

    Change the backDismiss option to accept the value 'auto'

--- src/helpers/modules/toggleBackDismiss.js
+++ src/helpers/modules/toggleBackDismiss.js
@@ -1,27 +1,37 @@
-import { CANCEL, OPTION_BACK_DISMISS, UI_EVENT_PREFIX } from "../constants";
+import {
+  AUTO,
+  CANCEL,
+  MODAL,
+  OPTION_BACK_DISMISS,
+  OPTION_LIGHT_DISMISS,
+  UI_EVENT_PREFIX,
+} from "../constants";
 import { isDialog } from "../is/index.js";
 import CloseWatcher from "../CloseWatcher.js";
 
 export default function (s, instance) {
-  const { base, on, opts, off } = instance;
+  const { base, on, off, opts } = instance;
 
   if (isDialog(base)) {
     if (s) {
       on(base, CANCEL + UI_EVENT_PREFIX, (event) => {
         event.preventDefault();
-        if (opts[OPTION_BACK_DISMISS]) {
-          instance.hide({ event });
-        }
       });
     } else {
       off(base, CANCEL + UI_EVENT_PREFIX);
     }
   }
   if (s) {
-    instance._closeWatcher = new CloseWatcher();
-    instance._closeWatcher.onclose = (event) => {
-      instance.hide({ event });
-    };
+    if (
+      opts[OPTION_BACK_DISMISS] === AUTO
+        ? opts[MODAL]
+        : opts[OPTION_BACK_DISMISS]
+    ) {
+      instance._closeWatcher = new CloseWatcher();
+      instance._closeWatcher.onclose = (event) => {
+        instance.hide({ event });
+      };
+    }
   } else {
     instance._closeWatcher?.destroy();
     instance._closeWatcher = null;
