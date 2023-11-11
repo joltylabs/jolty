85c5b4b09580ab4eb835a597edc562b1f110c96f
Anatolii Moldovanov <anmoldovanovdev@gmail.com>
2023-11-11 23:35:35 +0100

    fix lightDismiss keyup

--- src/helpers/utils/isClickOutsideElem.js
+++ src/helpers/utils/isClickOutsideElem.js
@@ -1,6 +1,7 @@
 import getBoundingClientRect from "../dom/getBoundingClientRect.js";
 
 export default (elem, { clientX, clientY }) => {
+  if (!clientX && !clientY) return false;
   const rect = getBoundingClientRect(elem);
   return (
     clientX < rect.left ||
